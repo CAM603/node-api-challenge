0bcbe985732773591b1d19ffc2ed62f0dfe69221
CAM603 <cameronhawley@comcast.net>
2020-02-14 12:49:59 -0500

    ADD delete action

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -53,4 +53,17 @@ router.put('/:id', (req, res) => {
             res.status(500).json({ error: 'Error updating action'})
         })
 })
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Action.remove(id)
+        .then(removed => {
+            res.status(200).json(removed)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error deleting action'})
+        })
+})
 module.exports = router;
