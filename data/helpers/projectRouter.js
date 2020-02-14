c5c8ffead82f0e73d7119ea6b1c171db54c5e99f
CAM603 <cameronhawley@comcast.net>
2020-02-14 12:06:22 -0500

    ADD get and post for projects

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -4,4 +4,41 @@ const Project = require('./projectModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+
+    Project.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error retrieving project'})
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Project.get(id)
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error retrieving project'})
+        })
+})
+
+router.post('/', (req, res) => {
+
+    Project.insert(req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error adding project'})
+        })
+})
+
 module.exports = router;
